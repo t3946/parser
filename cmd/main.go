2ec42b95d67cbde995aceb36397591932fd9d706
Vladislav <svialence@yandex.ru>
2025-07-03 20:07:37 +0300

    add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/joho/godotenv"
+	"parser/services/searchYandex"
+	"parser/services/storage"
 )
 
 func init() {
@@ -9,5 +12,11 @@ func init() {
 }
 
 func main() {
-
+	test := "1 keywords"
+	dataJson := storage.ReadFile("test/" + test + ".json")
+	var kw []string
+	json.Unmarshal([]byte(dataJson), &kw)
+	items, stats := searchYandex.ParseKeywordsList(kw, "46")
+	storage.WriteFile(test+"/result.json", items)
+	storage.WriteFile(test+"/stats.json", stats)
 }
