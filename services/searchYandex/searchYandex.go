b455908fa7c8424f74b4cf9572f8b67d5543192b
Vladislav <svialence@yandex.ru>
2025-07-11 16:29:02 +0300

    more headers to the Headers God

--- services/searchYandex/searchYandex.go
+++ services/searchYandex/searchYandex.go
@@ -138,14 +138,23 @@ func sleep(min, max float64) {
 
 func GetHeaders() map[string]string {
 	return map[string]string{
-		"User-Agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 		"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-		"Accept-Language":           "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-		"Connection":                "keep-alive",
-		"Upgrade-Insecure-Requests": "1",
-		"DNT":                       "1",
-		"Referer":                   "https://yandex.ru/",
 		"Accept-Encoding":           "gzip, deflate, br, zstd",
+		"Accept-Language":           "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+		"Cache-Control":             "no-cache",
+		"Connection":                "keep-alive",
+		"Host":                      "dzen.ru",
+		"Pragma":                    "no-cache",
+		"Sec-Fetch-Dest":            "document",
+		"Sec-Fetch-Mode":            "navigate",
+		"Sec-Fetch-Site":            "none",
+		"Sec-Fetch-User":            "?1",
+		"Upgrade-Insecure-Requests": "1",
+		"User-Agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+		"sec-ch-ua":                 "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
+		"sec-ch-ua-mobile":          "?0",
+		"sec-ch-ua-platform":        "Windows",
+		"Referer":                   "https://yandex.ru/",
 	}
 }
 
